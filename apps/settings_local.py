9fb1adc5fcdcdb132c762137d5a9ea44fe72d0c6
vladimir komarov <komarov-krsk@mail.ru>
2016-08-01 16:08:15 +0700

    start

---
+++ apps/settings_local.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+
+
+ROOT_URL = 'http://127.0.0.1:10000/'
+
+
+DATABASES = {
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_iss',
+        'USER': 'iss',
+        'PASSWORD': 'iss',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
+
+}
+
