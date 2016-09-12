c63b9af7a8fa06d0ceb983353cad2a3658acf920
vak <komarov-krsk@mail.ru>
2016-09-12 16:41:19 +0700

    fix path and name

---
+++ iss/settings_local.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+
+
+ROOT_URL = 'http://127.0.0.1:10000/'
+
+
+DATABASES = {
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'iss',
+        'USER': 'iss',
+	'PASSWORD':'iss',
+    },
+
+}
+
