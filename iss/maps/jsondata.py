8d4e3ebe3c06daa9dc30ffe3fbbefa2f32b756ad
vak <komarov-krsk@mail.ru>
2017-07-11 10:07:06 +0700

    Реализация интерфейса карт аварий

--- iss/maps/jsondata.py
+++ iss/maps/jsondata.py
@@ -11,6 +11,7 @@ from pprint import pformat
 from django.http import HttpResponse, HttpResponseRedirect
 
 from iss.localdicts.models import address_city, address_house
+from iss.monitor.models import accidents
 from django.shortcuts import redirect
 from django.core import serializers
 from django import template
@@ -44,14 +45,82 @@ def get_json(request):
             city_id = int(request.GET["city_id"],10)
 
             city = address_city.objects.get(pk=city_id)
-            r = address_house.objects.filter(city=city,house=None,street=None).first()
+            rec = address_house.objects.filter(city=city,house=None,street=None).first()
 
             data = {
-                "lat": r.geo["lat"],
-                "lng": r.geo["lng"]
+                "lat": rec.geo["lat"],
+                "lng": rec.geo["lng"]
             }
 
             response_data = data
+
+
+
+
+        ### Получение координат по аварии
+        if r.has_key("action") and rg("action") == 'get_accident_geo':
+            acc_id = int(request.GET["acc_id"],10)
+
+            acc = accidents.objects.get(pk=acc_id)
+            data = {}
+
+            data["acc_id"] = acc.id
+            data["acc_name"] = acc.acc_name
+            data["acc_reason"] = acc.acc_reason
+            data["address_list"] = []
+
+
+            ### Список id адресов
+            if acc.acc_addr_dict.has_key("address_list"):
+                addr_id_list = [a["address_id"] for a in acc.acc_addr_dict["address_list"]["address_list"]]
+            else:
+                addr_id_list = []
+
+
+            for add_id in addr_id_list:
+                add = address_house.objects.get(pk=add_id)
+                ### Проверка является ли адрес групповым
+                if add.house == None and add.street == None:
+                    for a in address_house.objects.filter(city=add.city).exclude(street=None).exclude(house=None):
+                        ### Добавление всех домов города или населенного пункта
+                        if a.geo["result"] == "ok":
+                            data["address_list"].append({
+                                "city": a.city.name,
+                                "street": a.street.name,
+                                "house": a.house,
+                                "lat": a.geo["lat"],
+                                "lng": a.geo["lng"]
+                            })
+
+                elif add.house == None and add.street:
+                    for b in address_house.objects.filter(city=add.city,street=add.street).exclude(house=None):
+                        ### Добавление всех домов на улице
+                        if b.geo["result"] == "ok":
+                            data["address_list"].append({
+                                "city": b.city.name,
+                                "street": b.street.name,
+                                "house": b.house,
+                                "lat": b.geo["lat"],
+                                "lng": b.geo["lng"]
+                            })
+
+                else:
+                ### Для обычных адресов домов
+                    if add.geo["result"] == "ok":
+                        data["address_list"].append({
+                            "city": add.city.name,
+                            "street": add.street.name,
+                            "house": add.house,
+                            "lat": add.geo["lat"],
+                            "lng": add.geo["lng"]
+                        })
+
+            
+
+
+            response_data = data
+
+
 
 
 
--- static/maps/accidents.js
+++ static/maps/accidents.js
@@ -3,12 +3,23 @@ $(document).ready(function() {
     // Сброс значений выбранного города
     $("#select-cities").val("0");
 
+    //$("table[group=accidents] tbody tr td a").bind("click",ShowAccident);
+    $("td").bind("click",ShowAccident);
+
+
     // Выбор города
     $( "#select-cities" ).change(function() {
 
         GetGeoCity();
 
     });
+
+
+
+    // Сброс значений выбора для варий
+    $("table[group=accidents] tbody tr").css("background-color","");
+    $("table[group=accidents] tbody tr").attr("chosen","no");
+
 
 
 $('table[group=accidents]').tableScroll({height:600});
@@ -19,13 +30,72 @@ $('table[group=accidents]').tableScroll({height:600});
 
 
 
+
+// Показать аварии на карте
+function ShowAccident(e) {
+
+    var partr = $(this).parent("tr");
+    var acc_id = partr.children("td").eq(0).text();
+    var status = partr.attr("chosen");
+
+    // Что то делаем если город выбран (отображен на карте)
+    if ( $("#select-cities").val() != "0" && status == "no" ) {
+
+        var jqxhr = $.getJSON("/maps/jsondata/?action=get_accident_geo&acc_id="+acc_id,
+            function(data) {
+
+            console.log(data);
+
+            // Обход набора координат
+            $.each(data["address_list"], function(key,value) {
+
+
+                // Создание метки
+                 point = new ymaps.Placemark([value["lat"], value["lng"]],
+                 {
+                    iconContent: data["acc_id"],
+                    hintContent: data["acc_reason"],
+                    balloonContentBody: value["city"] + ", " + value["street"] + " " + value["house"]
+                 },
+
+                 {
+                        preset: 'islands#redStretchyIcon'
+                 });
+
+                // Размещение на карте
+                 window.map.geoObjects.add(point);
+
+            });
+
+            partr.attr("chosen","yes");
+            partr.css("background-color","#F0E68C");
+
+        })
+
+    }
+
+}
+
+
+
+
+
+
+
 // Получение координат выбранного города или населенного пункта
 function GetGeoCity() {
 
     var city_id = $("#select-cities").val();
 
+    // Сброс значений выбора для варий
+    $("table[group=accidents] tbody tr").css("background-color","");
+    $("table[group=accidents] tbody tr").attr("chosen","no");
+
+
+
     var jqxhr = $.getJSON("/maps/jsondata/?action=get_city_geo&city_id="+city_id,
         function(data) {
+
 
 
             // Отрисовка карты
@@ -40,7 +110,6 @@ function GetGeoCity() {
                             });
 
                 });
-
 
 
         })
