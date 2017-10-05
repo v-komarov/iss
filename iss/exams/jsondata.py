15a6bba1c4f23cc336fb926d4c53143ee52f6fb2
vak <komarov-krsk@mail.ru>
2017-10-05 13:29:04 +0700

    Модуль тестирования: контроль повторного прохождения теста.

--- iss/exams/jsondata.py
+++ iss/exams/jsondata.py
@@ -241,7 +241,7 @@ def get_json(request):
 
 
             ### Проверка была ли сдача уже сегодня
-            if tests_results.objects.filter(worker__icontains=fio,begin__date=datetime.datetime.now().date(),ip=ip).exists():
+            if tests_results.objects.filter(test=t,worker__icontains=fio,begin__date=datetime.datetime.now().date(),ip=ip).exists():
 
                 response_data = {"result": "goaway"}
 
